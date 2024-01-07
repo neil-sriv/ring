dc209cee252a6d457c48a66195052977eafda8a5
Neil Srivastava <neil.srivastava1@gmail.com>
2024-01-07 00:43:01 -0800

    try to add client to docker

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-export const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- client/app/login/login.tsx
+++ client/app/login/login.tsx
@@ -1,6 +1,6 @@
 // import { inter } from "../layout";
 // import { inter } from "../layout";
-import { inter } from "../layout";
+// import { inter } from "../layout";
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -14,9 +14,7 @@ export default function LoginForm() {
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${inter.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+        <h1 className="top h1 mb-3 text-2xl">Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
