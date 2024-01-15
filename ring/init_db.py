9d8f5a9cc856d0de6ceb69a6afcabee5867288a8
Neil Srivastava <neil.srivastava1@gmail.com>
2024-01-14 16:20:07 -0800

    api + crud

--- ring/init_db.py
+++ ring/init_db.py
@@ -1,12 +1,7 @@
-from ring.postgres_models import User, Group, Letter, Question, Response
 from ring.sqlalchemy_base import Base, engine
 
 
 def init_db():
-    # for table in [User, Group, Letter, Question, Response]:
-    # if table.__table__.exists():
-    # table.__table__.drop(bind=engine)
-    # Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     pass
 
