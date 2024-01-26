9f831797e23ed97f49e453f63b19d8aad0cf2b53
Neil Srivastava <neil.srivastava1@gmail.com>
2024-01-25 19:53:25 -0800

    add schedule crud

--- ring/crud/group.py
+++ ring/crud/group.py
@@ -3,11 +3,11 @@ from typing import TYPE_CHECKING, Sequence
 from ring.crud import api_identifier as api_identifier_crud
 from ring.pydantic_schemas.schemas import GroupCreate
 from ring.postgres_models.group_model import Group
+from ring.postgres_models.user_model import User
 from sqlalchemy import select
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
-    from ring.postgres_models.user_model import User
 
 
 def get_groups(
@@ -17,12 +17,21 @@ def get_groups(
     if not user:
         raise Exception("User not found")
     return db.scalars(
-        select(Group).filter(Group.members.contains(user)).offset(skip).limit(limit)
+        select(Group)
+        .filter(
+            Group.members.contains(user),
+        )
+        .offset(skip)
+        .limit(limit)
     ).all()
 
 
 def create_group(db: Session, group: GroupCreate) -> Group:
-    admin_user = api_identifier_crud.get_model(db, User, api_id=group.admin_api_id)
+    admin_user = api_identifier_crud.get_model(
+        db,
+        User,
+        api_id=group.admin_api_id,
+    )
     if not admin_user:
         raise Exception("Admin user not found")
     db_letter = Group.create(group.name, admin_user)
---
+++ ring/crud/schedule.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Sequence
+from ring.crud import api_identifier as api_identifier_crud
+from ring.postgres_models.schedule_model import Schedule
+from ring.pydantic_schemas.schemas import GroupCreate
+from ring.postgres_models.group_model import Group
+from sqlalchemy import select
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+
+def get_schedule_for_group(db: Session, group_api_id: str) -> Schedule:
+    group = api_identifier_crud.get_model(db, Group, api_id=group_api_id)
+    if not group:
+        raise Exception("User not found")
+    return group.schedule
+
+
+# def create_schedule(db: Session, group: GroupCreate) -> Group:
+#     admin_user = api_identifier_crud.get_model(db, User, api_id=group.admin_api_id)
+#     if not admin_user:
+#         raise Exception("Admin user not found")
+#     db_letter = Group.create(group.name, admin_user)
+#     db.add(db_letter)
+#     db.commit()
+#     db.refresh(db_letter)
+#     return db_letter
