bbc43d9d1f45f1f664c332c382afc8b32ba093c7
Neil Srivastava <neil.srivastava1@gmail.com>
2024-02-24 23:17:25 -0800

    remove dynamo

--- ring/config.py
+++ ring/config.py
@@ -1,11 +1,9 @@
 from functools import lru_cache
-import os
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 
 class RingConfig(BaseSettings):
-    dynamo_db_host: str = f"http://{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}"
     environment: str
     sqlalchemy_database_uri: str
     JWT_SIGNING_KEY: str
