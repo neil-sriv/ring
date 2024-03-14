653bafa6d8a98922efdb1b309af3245ad6203f3a
Neil Srivastava <neil.srivastava1@gmail.com>
2024-03-13 20:15:22 -0700

    restart client work

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +11,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
