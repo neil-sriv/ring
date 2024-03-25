2ec0ce298cae585a3415688fe0676eb63aea80a9
Neil Srivastava <neil.srivastava1@gmail.com>
2024-03-24 19:18:16 -0700

    dev db

---
+++ dev_util/database.py
@@ -0,0 +1,18 @@
+import click
+from dev_util.dev import cmd_run, dev_group
+import os
+
+
+LOCAL_POSTGRES_URI = "postgresql://ring-postgres:ring-postgres@localhost:8004/ring"
+
+
+@dev_group("db")
+@click.pass_context
+def db(ctx: click.Context) -> None:
+    pass
+
+
+@cmd_run("pgcli", db)
+def db_pgcli() -> list[str]:
+    os.getenv("")
+    return ["pgcli", LOCAL_POSTGRES_URI]
