43003995b3ba26ebde77f0807ce7f37624737786
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-01 00:29:47 -0700

    prod script

---
+++ dev_util/prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+declare -a images=("ring-worker", "ring-api", "ring-next", "ring-beat", "nginx", "redis")
+
+for i in "${images[@]}"
+do
+    docker pull public.ecr.aws/z2k1e8p1/"$i":latest
+    docker tag public.ecr.aws/z2k1e8p1/"$i":latest "$i":latest
+    docker rmi public.ecr.aws/z2k1e8p1/"$i":latest
+done
