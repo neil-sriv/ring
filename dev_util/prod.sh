4dfc3d9e9b17dc74b51a9948b337120c26efff1f
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-01 00:32:18 -0700

    remove shared images

--- dev_util/prod.sh
+++ dev_util/prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a images=("ring-worker" "ring-api" "ring-next" "ring-beat" "nginx" "redis")
+declare -a images=("ring-worker" "ring-api" "ring-next" "ring-beat")
 
 for i in "${images[@]}"
 do
