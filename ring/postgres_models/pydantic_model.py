0da628997b87983c000b116cedd04abec34edf5e
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-05 01:47:13 -0700

    print pydantic models

--- ring/postgres_models/pydantic_model.py
+++ ring/postgres_models/pydantic_model.py
@@ -4,7 +4,10 @@ from pydantic import BaseModel
 class PydanticModel:
     PYDANTIC_MODEL: type[BaseModel]
 
-    def __repr__(self) -> str:
+    def to_pydantic(self) -> BaseModel:
+        return self.PYDANTIC_MODEL.model_validate(self)
+
+    def __str__(self) -> str:
         if self.PYDANTIC_MODEL:
-            return str(self.PYDANTIC_MODEL.model_validate(self))
-        return super().__repr__()
+            return str(self.PYDANTIC_MODEL.model_validate(self).model_dump())
+        return super().__str__()
