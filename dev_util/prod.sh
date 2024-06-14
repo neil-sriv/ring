e7e95c74bc4cba294bdcc76c3601c4eb23f2cbdd
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-14 16:32:30 -0700

    tag prod images

--- dev_util/prod.sh
+++ dev_util/prod.sh
@@ -5,6 +5,6 @@ declare -a images=("ring-worker" "ring-api" "ring-frontend" "ring-beat")
 for i in "${images[@]}"
 do
     docker pull public.ecr.aws/z2k1e8p1/"$i":latest
-    docker tag public.ecr.aws/z2k1e8p1/"$i":latest "$i":latest
+    docker tag public.ecr.aws/z2k1e8p1/"$i":latest "prod-$i":latest
     docker rmi public.ecr.aws/z2k1e8p1/"$i":latest
 done
