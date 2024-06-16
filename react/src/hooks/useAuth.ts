e20d5b5bc73102b6d34d4278860289cdc48abd43
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-15 18:32:12 -0700

    auto logout

--- react/src/hooks/useAuth.ts
+++ react/src/hooks/useAuth.ts
@@ -18,11 +18,20 @@ const isLoggedIn = () => {
 const useAuth = () => {
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
-  const { data: user, isLoading } = useQuery<UserLinked | null, Error>({
+  const {
+    data: user,
+    isLoading,
+    status,
+  } = useQuery<UserLinked | null, Error>({
     queryKey: ["currentUser"],
     queryFn: PartiesService.readUserMePartiesMeGet,
     enabled: isLoggedIn(),
   });
+
+  if (status === "error") {
+    localStorage.removeItem("access_token");
+    navigate({ to: "/login" });
+  }
 
   const login = async (data: AccessToken) => {
     const response = await LoginService.loginAccessTokenLoginAccessTokenPost({
