068e51af3f67477dc4414eabcaa3942e230a2596
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-21 23:48:35 -0700

    upsert sam

---
+++ ring/scripts/migration/upsert_users.py
@@ -0,0 +1,48 @@
+import json
+from pprint import pp
+from typing import Any, Sequence
+
+import sqlalchemy
+from ring.postgres_models.group_model import Group
+from ring.postgres_models.user_model import User
+from ring.sqlalchemy_base import Session
+from ring.scripts.script_base import script_di
+from ring.crud import user as user_crud
+import os
+
+
+@script_di()
+def run_script(db: Session, dry_run: bool = True) -> None:
+    with open(os.path.join(os.path.dirname(__file__), "users.json")) as f:
+        groups_dict: list[dict[str, Any]] = json.load(f)["groups"]
+    groups: Sequence[Group] = db.scalars(
+        sqlalchemy.select(Group).where(Group.name.in_([g["name"] for g in groups_dict]))
+    ).all()
+    for group in groups_dict:
+        new_users: list[User] = []
+        admin_email = group["admin"]
+        admin = user_crud.get_user_by_email(db, admin_email)
+        assert admin is not None, "Admin user not found"
+        [g] = [g for g in groups if g.name == group["name"]]
+        members = g.members
+
+        for user in group["members"]:
+            if user["email"] in [m.email for m in members]:
+                continue
+            u = user_crud.create_user(
+                db, user["email"], user["name"], "defaultPassword"
+            )
+            new_users.append(u)
+            g.members.append(u)
+            db.add(u)
+        for new_user in new_users:
+            pp(f"New user {new_user.name} added to group {g.name}")
+
+    for group in groups:
+        pp(str(group))
+
+    if dry_run:
+        pp("Dry run, rolling back")
+        db.rollback()
+    else:
+        db.commit()
