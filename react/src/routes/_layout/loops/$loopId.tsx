0a9ad8ad3a3b06d32403d06e8801f0f2b6affc73
Neil Srivastava <neil.srivastava1@gmail.com>
2024-06-21 22:11:02 -0700

    basic ass formatting

--- react/src/routes/_layout/loops/$loopId.tsx
+++ react/src/routes/_layout/loops/$loopId.tsx
@@ -40,8 +40,8 @@ export const Route = createFileRoute("/_layout/loops/$loopId")({
 
 function ResponseBlock({ response }: { response: ResponseWithParticipant }) {
   return (
-    <Box>
-      <Heading>{response.participant.name}</Heading>
+    <Box my="10px">
+      <Heading size="md">{response.participant.name}</Heading>
       <Text>{response.response_text}</Text>
     </Box>
   );
@@ -49,10 +49,12 @@ function ResponseBlock({ response }: { response: ResponseWithParticipant }) {
 
 function QuestionBlock({ question }: { question: PublicQuestion }) {
   return (
-    <Box>
+    <Box my="20px">
       <Heading>{question.question_text}</Heading>
       {question.responses.map((response) => {
-        return <ResponseBlock response={response} />;
+        return (
+          <ResponseBlock response={response} key={response.api_identifier} />
+        );
       })}
     </Box>
   );
@@ -74,7 +76,9 @@ function IssueContent() {
       </Container> */}
       <Container>
         {loop.questions.map((question) => {
-          return <QuestionBlock question={question} />;
+          return (
+            <QuestionBlock question={question} key={question.api_identifier} />
+          );
         })}
       </Container>
     </Container>
