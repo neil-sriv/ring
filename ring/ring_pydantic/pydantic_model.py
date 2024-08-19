472be04d0e792ec2cd09de24fbb40972d9f30f19
Neil Srivastava <neil.srivastava1@gmail.com>
2024-08-18 23:29:50 -0700

    better repr

--- ring/ring_pydantic/pydantic_model.py
+++ ring/ring_pydantic/pydantic_model.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel
 
 
@@ -11,3 +12,11 @@ class PydanticModel:
         if self.PYDANTIC_MODEL:
             return str(self.PYDANTIC_MODEL.model_validate(self).model_dump())
         return super().__str__()
+
+    # def __repr__(self) -> str:
+    #     if self.PYDANTIC_MODEL:
+    #         return str(self.PYDANTIC_MODEL.model_validate(self).model_dump())
+    #     return super().__repr__()
+
+    def __repr__(self) -> str:
+        return json.dumps(self.__dict__, indent=4, default=str)
