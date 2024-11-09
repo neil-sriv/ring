1de1529754eaccfbf7080184f997cbdd7664388c
Neil Srivastava <neil.srivastava1@gmail.com>
2024-11-09 00:12:06 -0800

    update schemas

---
+++ ring/parties/schemas/one_time_token.py
@@ -0,0 +1,16 @@
+from typing import Any
+from pydantic import Field, computed_field
+
+
+class WithTokenMixin:
+    one_time_token: Any = Field(exclude=True)
+
+    @computed_field
+    @property
+    def token(self) -> str:
+        return self.one_time_token.token
+
+    @computed_field
+    @property
+    def is_expired(self) -> bool:
+        return self.one_time_token.is_expired
