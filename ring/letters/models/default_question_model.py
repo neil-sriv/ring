b3de6d63db943a79c15c2cf169cbc359495a0586
Neil Srivastava <neil.srivastava@benchling.com>
2024-12-13 18:27:52 -0800

    fix circular dependency

--- ring/letters/models/default_question_model.py
+++ ring/letters/models/default_question_model.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ring.api_identifier.api_identified_model import APIIdentified
 from ring.created_at import CreatedAtMixin
-from ring.parties.models.group_model import Group
 from ring.sqlalchemy_base import Base
+
+if TYPE_CHECKING:
+    from ring.parties.models.group_model import Group
 
 
 class DefaultQuestion(Base, APIIdentified, CreatedAtMixin):
