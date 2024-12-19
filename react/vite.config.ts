0f380a983bda5d6a5e7cfe8593df4f9e35d2ebd3
Neil Srivastava <neil.srivastava1@gmail.com>
2024-12-19 04:49:30 -0800

    adds very basic PWA support (#79)

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,8 +1,13 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
 });
