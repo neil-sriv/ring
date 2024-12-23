afaa9ec5266149437e539a71bf85e0adf05119f8
Neil Srivastava <neil.srivastava@benchling.com>
2024-12-23 12:14:12 -0800

    user crud tests

--- ring/tests/factories/parties/user_factory.py
+++ ring/tests/factories/parties/user_factory.py
@@ -1,6 +1,9 @@
-from factory import Faker
+from typing import Any
+
+from factory import Faker, post_generation
 
 from ring.parties.models.user_model import User
+from ring.security import get_password_hash
 from ring.tests.factories.base_factory import BaseFactory
 
 
@@ -9,6 +12,15 @@ class UserFactory(BaseFactory[User]):
         model = User
         sqlalchemy_session_persistence = "flush"
 
+    @post_generation
+    def password(
+        obj, create: bool, extracted: str | None, **kwargs: Any
+    ) -> None:
+        password = extracted or "password"
+        obj.hashed_password = get_password_hash(password)
+
     email = Faker("email")
     name = Faker("name")
+
+    # Temporary value until post generation function is called
     hashed_password = Faker("password")
--- ring/tests/unit/parties/crud/user_test.py
+++ ring/tests/unit/parties/crud/user_test.py
@@ -1,17 +1,63 @@
+from factory.faker import Faker
 from sqlalchemy.orm import Session
 
 from ring.parties.crud import user as user_crud
 from ring.tests.factories.parties.user_factory import UserFactory
 
 
-def test_get_users(db_session: Session):
-    users = user_crud.get_users(db_session)
+class TestUserCrud:
+    def test_get_users(self, db_session: Session) -> None:
+        assert user_crud.get_users(db_session) == []
 
-    assert len(users) == 0
+        db_users = [UserFactory.create() for _ in range(10)]
+        db_session.commit()
 
-    test = [UserFactory.create() for _ in range(10)]
-    assert len(test) == 10
-    db_session.commit()
+        users = user_crud.get_users(db_session)
+        assert len(users) == 10
+        assert users == db_users
 
-    users = user_crud.get_users(db_session)
-    assert len(users) == 10
+    def test_authenticate_user(self, db_session: Session) -> None:
+        user = UserFactory.create(password="test password")
+        db_session.commit()
+
+        assert user == user_crud.authenticate_user(
+            db_session, user.email, "test password"
+        )
+
+    def test_authenticate_user_invalid(self, db_session: Session) -> None:
+        user = UserFactory.create(password="test password")
+        db_session.commit()
+
+        assert (
+            user_crud.authenticate_user(
+                db_session, user.email, "invalid password"
+            )
+            is None
+        )
+        assert (
+            user_crud.authenticate_user(
+                db_session, "invalid email", "test password"
+            )
+            is None
+        )
+
+    def test_get_user_by_email(self, db_session: Session) -> None:
+        user = UserFactory.create(email="test_email")
+        db_session.commit()
+
+        assert user_crud.get_user_by_email(db_session, "test_email") == user
+
+    def test_get_user_by_email_invalid(self, db_session: Session) -> None:
+        UserFactory.create()
+        db_session.commit()
+
+        assert user_crud.get_user_by_email(db_session, "invalid_email") is None
+
+    def test_create_user(self, db_session: Session, faker: Faker) -> None:
+        email, name, password = faker.email(), faker.name(), faker.password()
+        user = user_crud.create_user(db_session, email, name, password)
+        db_session.commit()
+
+        assert user.email == email
+        assert user.name == name
+        assert user_crud.authenticate_user(db_session, user.email, password)
