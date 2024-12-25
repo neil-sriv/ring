a290ccf6bc25abf8333703959c791a22af30938c
Neil Srivastava <neil.srivastava@benchling.com>
2024-12-25 13:06:35 -0500

    user model relationship test

--- ring/tests/factories/entrypoint/hydrate_registries.py
+++ ring/tests/factories/entrypoint/hydrate_registries.py
@@ -1,4 +1,7 @@
 def hydrate_factories_registry():
+    from ring.tests.factories.letters.letter_factory import LetterFactory
+    from ring.tests.factories.letters.question_factory import QuestionFactory
+    from ring.tests.factories.letters.response_factory import ResponseFactory
     from ring.tests.factories.parties.group_factory import GroupFactory
     from ring.tests.factories.parties.invite_factory import InviteFactory
     from ring.tests.factories.parties.one_time_token_factory import (
--- ring/tests/unit/parties/models/user_model_test.py
+++ ring/tests/unit/parties/models/user_model_test.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 
 from ring.parties.crud.user import get_password_hash
 from ring.parties.models.user_model import User
+from ring.tests.factories.letters.response_factory import ResponseFactory
+from ring.tests.factories.parties.group_factory import GroupFactory
 
 
 class TestUserModel:
@@ -17,7 +19,7 @@ class TestUserModel:
         assert user.email == email
         assert user.name == "test"
         assert user.hashed_password == hashed_password
-        assert user.api_identifier.startswith("usr")
+        assert user.api_identifier.startswith(User.API_ID_PREFIX)
         assert user.id is not None
         assert user.created_at is not None
 
@@ -25,3 +27,36 @@ class TestUserModel:
             sqlalchemy.select(User).filter(User.email == email)
         ).one()
         assert db_user == user
+
+    def test_user_model_with_group(
+        self, faker: Faker, db_session: Session
+    ) -> None:
+        hashed_password = get_password_hash("password")
+        email = faker.email()
+        user = User(email=email, name="test", hashed_password=hashed_password)
+        db_session.add(user)
+        db_session.commit()
+
+        assert user.groups == []
+
+        group = GroupFactory.create()
+        user.groups = [group]
+        db_session.commit()
+
+        assert user.groups == [group]
+
+    def test_user_model_with_response(
+        self, faker: Faker, db_session: Session
+    ) -> None:
+        hashed_password = get_password_hash("password")
+        email = faker.email()
+        user = User(email=email, name="test", hashed_password=hashed_password)
+        db_session.add(user)
+        db_session.commit()
+
+        assert user.responses == []
+
+        response = ResponseFactory.create(participant=user)
+        db_session.commit()
+
+        assert user.responses == [response]
