9ebefaf3f358d2c23479611f79faa9e53cb20c61
Neil Srivastava <neil.srivastava1@gmail.com>
2025-02-23 05:28:27 -0500

    include responder information for all letters (backend and FE client)

--- ring/ring_pydantic/linked_schemas.py
+++ ring/ring_pydantic/linked_schemas.py
@@ -47,6 +47,7 @@ class LetterLinked(Letter):
 class PublicLetter(Letter):
     group: "GroupUnlinked"
     questions: list["PublicQuestion"]
+    responders: list["UserUnlinked"]
 
 
 class DashboardLetters(BaseModel):
