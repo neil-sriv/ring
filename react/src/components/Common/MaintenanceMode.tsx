64d222897a698e0566cdf7456b84b55a3de1bdaa
Neil Srivastava <neil.srivastava1@gmail.com>
2025-03-30 11:14:39 -0400

    downtime mode on FE (#146)

---
+++ react/src/components/Common/MaintenanceMode.tsx
@@ -0,0 +1,15 @@
+import { Container, Heading, Text, VStack } from "@chakra-ui/react";
+
+export default function MaintenanceMode() {
+  return (
+    <Container maxW="container.md" py={20}>
+      <VStack spacing={8} textAlign="center">
+        <Heading size="2xl">🛠️ Maintenance Mode</Heading>
+        <Text fontSize="xl">
+          We're currently performing scheduled maintenance to improve our infrastructure.
+          Please check back later.
+        </Text>
+      </VStack>
+    </Container>
+  );
+} 
