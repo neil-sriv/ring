316b75c3a7c20888d66475372f9026a48b614392
Neil Srivastava <neil.srivastava1@gmail.com>
2025-03-30 12:14:53 -0400

    vercel logging test (#148)

--- react/src/util/env.ts
+++ react/src/util/env.ts
@@ -1,4 +1,5 @@
 export function isMaintenanceMode() {
-  const mode = import.meta.env.VITE_MAINTENANCE_MODE;
-  return mode === "true" || mode === true;
+    console.log(import.meta.env);
+    const mode = import.meta.env.VITE_MAINTENANCE_MODE;
+    return mode === "true" || mode === true;
 } 
