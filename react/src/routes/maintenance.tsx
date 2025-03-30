c57282f4bf1ae98e3e2e1c30c8517ad9df9c16be
Neil Srivastava <neil.srivastava1@gmail.com>
2025-03-30 11:35:20 -0400

    prod frontend container use env file (#147)

--- react/src/routes/maintenance.tsx
+++ react/src/routes/maintenance.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import MaintenanceMode from "../components/Common/MaintenanceMode";
+import { isMaintenanceMode } from "../util/env";
 
 export const Route = createFileRoute("/maintenance")({
   component: MaintenanceMode,
   beforeLoad: () => {
     // If maintenance mode is disabled, redirect to home
-    if (!import.meta.env.VITE_MAINTENANCE_MODE) {
+    if (!isMaintenanceMode()) {
       throw new Error("Maintenance mode is disabled");
     }
     return {};
---
+++ react/src/util/env.ts
@@ -0,0 +1,4 @@
+export function isMaintenanceMode() {
+  const mode = import.meta.env.VITE_MAINTENANCE_MODE;
+  return mode === "true" || mode === true;
+} 
