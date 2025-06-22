e10e5721bcf9a381413a2a14d2997e6466ff7c65
Neil Srivastava <neil.srivastava1@gmail.com>
2025-06-21 21:23:36 -0400

    initial pycasbin model.conf and example policy

--- ring/authz/enforcer.py
+++ ring/authz/enforcer.py
@@ -5,4 +5,4 @@ from casbin import Enforcer, util
 
 def get_enforcer() -> Enforcer:
     """Get the enforcer for the authz system."""
-    return Enforcer("authz/model.conf", "authz/policy.csv")
+    return Enforcer("/src/ring/authz/model.conf", "/src/ring/authz/policy.csv")
