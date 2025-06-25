979d85e953f9cf290dbd2f142e4959b81637adf4
Neil Srivastava <neil.srivastava1@gmail.com>
2025-06-24 20:20:30 -0400

    fix outer joins and give groups access to their users

--- ring/authz/enforcer.py
+++ ring/authz/enforcer.py
@@ -32,41 +32,61 @@ def build_stateless_enforcer(db: Session, sub_api_id: str) -> Enforcer:
     """Build a stateless enforcer for a request."""
     enforcer = get_enforcer()
 
-    user = get_model(db, User, sub_api_id)
-    user_groups = user.groups
-    group_api_ids = [group.api_identifier for group in user_groups]
-    for group_api_id in group_api_ids:
-        enforcer.add_grouping_policy(sub_api_id, group_api_id)
+    # add g1 rules for user groups
+    stmt = (
+        select(Group.api_identifier, User.api_identifier)
+        .join(Group.members)
+        .where(Group.members.any(User.api_identifier == sub_api_id))
+    )
+    user_groups = db.execute(stmt).all()
+
+    for group_api_id, user_api_id in user_groups:
+        enforcer.add_grouping_policy(user_api_id, group_api_id)
 
     # add g2 rules for resource hierarchy
     stmt = (
         select(
             Group.api_identifier,
+            User.api_identifier,
             Letter.api_identifier,
             Question.api_identifier,
             Response.api_identifier,
         )
-        .join(Group.letters)
-        .join(Letter.questions)
-        .join(Question.responses)
-        .where(Group.api_identifier.in_(group_api_ids))
+        .outerjoin(Group.letters)
+        .outerjoin(Group.members)
+        .outerjoin(Letter.questions)
+        .outerjoin(Question.responses)
+        .where(
+            Group.api_identifier.in_(
+                [group_api_id for group_api_id, _ in user_groups]
+            )
+        )
     )
     resources = db.execute(stmt).all()
 
     for (
         group_api_id,
+        user_api_id,
         letter_api_id,
         question_api_id,
         response_api_id,
     ) in resources:
-        enforcer.add_named_grouping_policy("g2", letter_api_id, group_api_id)
-        enforcer.add_named_grouping_policy(
-            "g2", question_api_id, letter_api_id
-        )
-        enforcer.add_named_grouping_policy(
-            "g2", response_api_id, question_api_id
-        )
+        if user_api_id:
+            enforcer.add_named_grouping_policy("g2", user_api_id, group_api_id)
+        if letter_api_id:
+            enforcer.add_named_grouping_policy(
+                "g2", letter_api_id, group_api_id
+            )
+        if question_api_id:
+            enforcer.add_named_grouping_policy(
+                "g2", question_api_id, letter_api_id
+            )
+        if response_api_id:
+            enforcer.add_named_grouping_policy(
+                "g2", response_api_id, question_api_id
+            )
 
+    # add p rules for group permissions
     for group in user_groups:
         enforcer.add_policy(
             group.api_identifier, group.api_identifier, Action.READ.value
